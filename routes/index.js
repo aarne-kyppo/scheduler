38c5e99a02d18ee4d9d676f1fdd4a7868e15a6aa
Aarne Kyppö <kyppoa@gmail.com>
2015-03-12 00:07:07 +0200

    UI fixes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var _ = require('underscore');
+var mongo = require('mongojs').connect('schedule',['lessons',]);
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var hours = _.range(8,21);
+    res.render('index',{hours: hours, title: 'Scheduler'});
 });
 
+router.get('/lessons/test',function(req,res,next){
+    mongo.lessons.find({start_date: '2015-05-11'},function(err,lessons){
+        res.json(lessons);
+    });
+});
 module.exports = router;
